2717ac8d54bfaea6782ca96c501198fb69a6ec82
jarvisdemobot1 <vinod.batta@jiffy.ai>
2023-06-25 13:10:29 +0530

    Added a new java file

---
+++ src/samplepackage/Reverse.java
@@ -0,0 +1,11 @@
+package samplepackage;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str="Vinod";
+        str.length();
+	}
+
+}
